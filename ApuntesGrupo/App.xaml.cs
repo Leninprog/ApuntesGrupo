e40b34db0a1b27f8c46e2efe8191e3fe9bdbfd6c
laboratorio <laboratorio@UPOAULA10702>
2025-06-16 08:16:41 -0500

    Agregar archivos de proyecto.

---
+++ ApuntesGrupo/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ApuntesGrupo
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ ApuntesGrupo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ApuntesGrupo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
